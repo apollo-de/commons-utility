dca32dc5784b9266a0e0ab9eb52b57e48a7f43a1
shylock <shylock@apollo>
2025-06-24 11:00:22 +0200

    initial commit

---
+++ src/main/java/de/gupta/commons/utility/map/MapSorter.java
@@ -0,0 +1,26 @@
+package de.gupta.commons.utility.map;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public final class MapSorter
+{
+	public static <V> SortedMap<LocalDate, V> sortMapByLocalDateKey(final Map<LocalDate, V> map)
+	{
+		return sortMapByKey(map, Comparator.naturalOrder());
+	}
+
+	public static <K, V> SortedMap<K, V> sortMapByKey(final Map<K, V> map, final Comparator<? super K> keyComparator)
+	{
+		SortedMap<K, V> sortedMap = new TreeMap<>(keyComparator);
+		sortedMap.putAll(map);
+		return sortedMap;
+	}
+
+	private MapSorter()
+	{
+	}
+}
