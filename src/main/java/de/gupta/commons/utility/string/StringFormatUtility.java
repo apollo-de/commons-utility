4a46730069b61985003f590829b6ba6cc55c4041
shylock <shylock@apollo>
2025-07-04 10:36:08 +0200

    String utility method made functional
    Took 10 minutes

--- src/main/java/de/gupta/commons/utility/string/StringFormatUtility.java
+++ src/main/java/de/gupta/commons/utility/string/StringFormatUtility.java
@@ -1,5 +1,7 @@
 package de.gupta.commons.utility.string;
 
+import java.util.Optional;
+
 public final class StringFormatUtility
 {
 	private StringFormatUtility()
@@ -13,54 +15,10 @@ public final class StringFormatUtility
 
 	public static boolean hasValidJavaClassNameFormat(String domainName)
 	{
-		if (domainName.isEmpty())
-		{
-			return false;
-		}
-
-		boolean expectLower = true;
-		boolean hasLowercase = false;
-
-		for (int i = 0; i < domainName.length(); i++)
-		{
-			char c = domainName.charAt(i);
-
-			// Only ASCII letters allowed (a-z, A-Z)
-			if (!Character.isLetter(c) || c > 127)
-			{
-				return false;
-			}
-
-			// First letter must be uppercase
-			if (i == 0)
-			{
-				if (!Character.isUpperCase(c))
-				{
-					return false;
-				}
-				expectLower = true;
-				continue;
-			}
-
-			if (Character.isUpperCase(c))
-			{
-				// Uppercase in the middle is only allowed for new words
-				expectLower = true;
-			}
-			else if (expectLower)
-			{
-				// After uppercase, we expect lowercase
-				expectLower = false;
-				hasLowercase = true;
-			}
-			else
-			{
-				hasLowercase = true;
-			}
-		}
-
-		// Valid Java class names should have at least one lowercase letter (camel case)
-		// Special case: a single uppercase letter is also valid
-		return hasLowercase || domainName.length() == 1;
+		return Optional.ofNullable(domainName)
+					   .filter(StringFormatUtility::startsWithUppercase)
+					   .filter(name -> name.chars().allMatch(c -> Character.isLetter(c) && c <= 127))
+					   .filter(name -> name.length() == 1 || name.chars().anyMatch(Character::isLowerCase))
+					   .isPresent();
 	}
 }
